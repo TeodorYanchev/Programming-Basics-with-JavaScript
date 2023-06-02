9adb32f0c4ed21d369cd6ed60cca1dc6fed1b308
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-02 21:00:38 +0300

    Add files via upload

---
+++ basic-exe/cinemaTicket.js
@@ -0,0 +1,17 @@
+function cinemaTicket(input) {
+    let ticket = input[0];
+
+    switch (ticket) {
+        case "Monday":
+        case "Tuesday":
+        case "Friday": console.log(12); break;
+        case "Wednesday":
+        case "Thursday": console.log(14); break;
+        case "Saturday":
+        case "Sunday": console.log(16); break;   
+    }
+
+
+}
+
+cinemaTicket(["Monday"])
