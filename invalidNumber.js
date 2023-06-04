4b9a0f050dd3f7a9b8e677b25c7016b518c0f5ab
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-04 21:16:49 +0300

    Add files via upload

---
+++ invalidNumber.js
@@ -0,0 +1,13 @@
+function invalidNumber(input) {
+    let n = Number(input[0]);
+
+    let isValid = n >= 100 && n <= 200 || n == 0;
+
+    if (!isValid) {
+        console.log("invalid");
+    }
+}
+
+
+
+invalidNumber(["220"])
