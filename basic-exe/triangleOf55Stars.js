cfd6d07f05ca696fc633056957cd9bec88ad8034
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-07 11:50:12 +0300

    Add files via upload

---
+++ basic-exe/triangleOf55Stars.js
@@ -0,0 +1,15 @@
+function triangleOf55Stars() {
+    console.log("*");
+    console.log("**");
+    console.log("***");
+    console.log("****");
+    console.log("*****");
+    console.log("******");
+    console.log("*******");
+    console.log("********");
+    console.log("*********");
+    console.log("**********");
+
+}
+
+triangleOf55Stars()
