bbfc98a59ded122e0124a2172262f1b097a76743
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-08 13:01:38 +0300

    Add files via upload

--- basic-exe/concatenateData.js
+++ basic-exe/concatenateData.js
@@ -1,11 +1,5 @@
 function concatenateData(input) {
-    let firstName = input[0];
-    let lastName = input[1];
-    let age = Number(input[2]);
-    let town = input[3];
 
-    //"You are <firstName> <lastName>, a <age>-years old person from <town>."
-    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`)
 }
 
-concatenateData(["Teodor", "Tenev", "32", "Bourgas"])
+concatenateData
