0c16890c4241fc0f9f9eabd4bf202f811962465c
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-09 09:28:43 +0300

    Add files via upload

--- basic-exe/inchesToCentimeters.js
+++ basic-exe/inchesToCentimeters.js
@@ -1,10 +1,7 @@
-function inchesToCentimeters(input) {
-
-    let inches = Number(input[0]);
-    centimeters = inches * 2.54
-    console.log(centimeters)
-
-
+function inchesToCentimeters(input){
+let inches = Number(input[0]);
+let centimeters = inches * 2.54;
+console.log('Centimeters = ' + centimeters);
 }
 
-inchesToCentimeters(["7"])
+inchesToCentimeters([5])
