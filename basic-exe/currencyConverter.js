cc1d7a6f3d448f59708713a8e06c14f64f9d8a69
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-09 09:28:11 +0300

    Add files via upload

---
+++ basic-exe/currencyConverter.js
@@ -0,0 +1,11 @@
+function currencyConverter(input){
+    let val = Number(input[0]);
+    let bgn = 1;
+    let usd = 1.79549;
+    let eur = 1.95583;
+    let gbp = 2.53405;
+
+
+}
+
+currencyConverter([])
