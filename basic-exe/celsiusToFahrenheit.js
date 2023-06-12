9b6c993fdaea33aa19ac27bcff22f3ffa8d8d12e
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-12 23:09:02 +0300

    Add files via upload

--- basic-exe/celsiusToFahrenheit.js
+++ basic-exe/celsiusToFahrenheit.js
@@ -1,7 +1,8 @@
 function celsiusToFahrenheit(input){
-let celsius = Number(input[0]);
-let fahrenheit = (celsius * 9/5) +32
-console.log(fahrenheit);
+    let celsius = Number(input[0]);
+    let fahrenheit = (celsius * 9/5) +32;
+    console.log(fahrenheit.toFixed(2));
+
 }
 
-celsiusToFahrenheit(["25"])
+celsiusToFahrenheit(["0"])
