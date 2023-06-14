c2562d70b08ea75cb3dc58f1f53062372fb83843
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-14 19:58:24 +0300

    Add files via upload

--- basic-exe/triangleArea.js
+++ basic-exe/triangleArea.js
@@ -1,10 +1,12 @@
-function triangleArea(input){
+function triangleArea(input) {
     let a = Number(input[0]);
     let h = Number(input[1]);
-    
+
     let area = a * h / 2;
     console.log(area.toFixed(2));
 
 }
 
-triangleArea(["20", "30"])
+triangleArea(["1.23456",
+    "4.56789"    
+    ])
