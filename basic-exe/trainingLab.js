bfda3b53fb1b65c86e3a30ad31504f2a9acaf23f
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-14 19:57:28 +0300

    Add files via upload

---
+++ basic-exe/trainingLab.js
@@ -0,0 +1,15 @@
+function trainingLab(input){
+
+    let w = Number(input[0]);
+    let h = Number(input[1]);
+
+    let desk = h - 100;
+    let deskOnR = (desk * 70) + 20;
+
+    let redove = w -
+
+
+}
+
+trainingLab(["15",
+    "8.9"])
