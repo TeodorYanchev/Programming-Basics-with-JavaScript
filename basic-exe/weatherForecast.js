c1af7eb708a767c9eaf6df70522526c2eb306d8f
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-15 22:21:15 +0300

    Add files via upload

---
+++ basic-exe/weatherForecast.js
@@ -0,0 +1,6 @@
+function weatherForecast([firstName, lastName, age, town]) {
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+
+}
+
+weatherForecast(['Ivan', 'Ivanov', 20, 'Sofia'])
