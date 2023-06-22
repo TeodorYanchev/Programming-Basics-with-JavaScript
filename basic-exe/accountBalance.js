e277d5fc6929685e303faa46438bee2d5136f030
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-06-22 08:23:53 +0300

    Add files via upload

---
+++ basic-exe/accountBalance.js
@@ -0,0 +1,26 @@
+function accountBalance(input) {
+    let account = 0;
+
+    let index = 0;
+    let command = input[index];
+    index++;
+
+    while(command !== "NoMoreMoney") {
+        let money = Number(command);
+        if (money < 0) {
+            console.log("Invalid operation!");
+            break;
+        }
+        account += money;
+        console.log(`Increase: ${money.toFixed(2)}`);
+        command = input[index];
+        index++;
+    }
+    console.log(`Total: ${account.toFixed(2)}`);
+
+}
+
+accountBalance(["5.51", 
+"69.42",
+"100",
+"NoMoreMoney"])
