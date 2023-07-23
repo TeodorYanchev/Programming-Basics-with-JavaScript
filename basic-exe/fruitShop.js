6c0ec203b3ba33e1b288dcd62177f23a16d85010
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-07-23 23:02:52 +0300

    Add files via upload

--- basic-exe/fruitShop.js
+++ basic-exe/fruitShop.js
@@ -1,34 +1,72 @@
-function fruitShop(input) {
+function fruitShopPrice(input) {
     let fruit = input[0];
     let dayOfWeek = input[1];
     let quantity = Number(input[2]);
     let price = 0;
+  
+    if (dayOfWeek === 'Monday' || dayOfWeek === 'Tuesday' || dayOfWeek === 'Wednesday' || dayOfWeek === 'Thursday' || dayOfWeek === 'Friday') {
+      switch (fruit) {
+        case 'banana':
+          price = 2.50;
+          break;
+        case 'apple':
+          price = 1.20;
+          break;
+        case 'orange':
+          price = 0.85;
+          break;
+        case 'grapefruit':
+          price = 1.45;
+          break;
+        case 'kiwi':
+          price = 2.70;
+          break;
+        case 'pineapple':
+          price = 5.50;
+          break;
+        case 'grapes':
+          price = 3.85;
+          break;
+        default:
+          console.log('error');
+          return;
+      }
+    } else if (dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday') {
+      switch (fruit) {
+        case 'banana':
+          price = 2.70;
+          break;
+        case 'apple':
+          price = 1.25;
+          break;
+        case 'orange':
+          price = 0.90;
+          break;
+        case 'grapefruit':
+          price = 1.60;
+          break;
+        case 'kiwi':
+          price = 3.00;
+          break;
+        case 'pineapple':
+          price = 5.60;
+          break;
+        case 'grapes':
+          price = 4.20;
+          break;
+        default:
+          console.log('error');
+          return;
+      }
+    } else {
+      console.log('error');
+      return;
+    }
+  
+    let totalPrice = price * quantity;
+    console.log(totalPrice.toFixed(2));
 
-
-   if(dayOfWeek == "Monday") {
-    if(fruit == "banana") {
-        price = quantity * 2.50;
-    } else if(fruit == "apple") {
-        price = quantity * 1.20;
-    } else if(fruit == "orange") {
-        price = quantity * 0.85;
-    } else if(fruit == "grapefruit") {
-        price = quantity * 1.45;
-    } else if(fruit == "kiwi") {
-        price = quantity * 2.70;
-    } else if(fruit == "pineapple") {
-        price = quantity * 5.50;
-    } else if(fruit == "grapes") {
-        price = quantity * 3.85;
-    } console.log(price.toFixed(2));
-   } 
-
-
-
-    
-}
-
-fruitShop(["kiwi",
-"Monday",
-"2.5"])
+fruitShop(["apple",
+"Tuesday",
+"2"])
 
