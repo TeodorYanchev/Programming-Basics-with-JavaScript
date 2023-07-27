69d963648935c244d3893f144d281c200c4f9cc6
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-07-27 23:19:09 +0300

    Add files via upload

--- basic-exe/multiplicationTable.js
+++ basic-exe/multiplicationTable.js
@@ -1,10 +1,10 @@
 function multiplicationTable(input) {
-    let num = Number(input[0]);
-
-    for(let i = 1; i <= 10; i++){
-        console.log(`${i} * ${num} = ${i * num}`);
+    for (let x = 1; x <= 10; x++) {
+        for (let y = 1; y <= 10; y++){
+            console.log(`${x} * ${y} = ${x * y}`);
+    }
     }
 
 }
 
-multiplicationTable(["5"])
+multiplicationTable()
