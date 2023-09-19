c9227ee0df01a21d41079dce927d16d3443c08bd
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-09-19 19:28:56 +0300

    Add files via upload

---
+++ basic-exe/foreignLanguages.js
@@ -0,0 +1,15 @@
+function foreignLanguages(country) {
+    if (country === "USA") {
+        console.log("English");
+    } else if (country === "England") {
+        console.log("English");
+    } else if (country === "Spain") {
+        console.log("Spanish");
+    } else if (country === "Argentina") {
+        console.log("Spanish");
+    } else if (country === "Mexico") {
+        console.log("Spanish");
+    } else {
+        console.log("unknown");
+    }
+}
