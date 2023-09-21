b3e4b6541c1edc6c84df61775c86a9fb13c4133c
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-09-21 12:23:03 +0300

    Add files via upload

---
+++ basic-exe/theatrePromotions.js
@@ -0,0 +1,27 @@
+function theatrePromotions(day, age) {
+    let price = 0;
+    if (age >= 0 && age <= 18) {
+        switch (day) {
+            case "Weekday": price = 12; break;
+            case "Weekend": price = 15; break;
+            case "Holiday": price = 5; break;
+        }
+    } else if (age > 18 && age <= 64) {
+        switch (day) {
+            case "Weekday": price = 18; break;
+            case "Weekend": price = 20; break;
+            case "Holiday": price = 12; break;
+        }
+    } else if (age > 64 && age <= 122) {
+        switch (day) {
+            case "Weekday": price = 12; break;
+            case "Weekend": price = 15; break;
+            case "Holiday": price = 10; break;
+        }
+    }
+    if (price !== 0) {
+        console.log(price + "$");
+    } else {
+        console.log("Error!");
+    }
+}
