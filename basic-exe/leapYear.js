f977287c395870a72bc35108f56549e3c21888ea
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-09-22 09:20:25 +0300

    Add files via upload

---
+++ basic-exe/leapYear.js
@@ -0,0 +1,9 @@
+function leapYear(year) {
+    let isLeap = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+    if(isLeap) {
+        console.log('yes');
+    } else {
+        console.log('no');
+    }
+}
