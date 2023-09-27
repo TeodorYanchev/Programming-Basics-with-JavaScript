e222bd33f4e0c49e05a5c4caecfe897fb33984ae
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-09-27 13:35:28 +0300

    Add files via upload

---
+++ basic-exe/calculator .js
@@ -0,0 +1,11 @@
+function calculator (firstNum, operator, secondNum) {
+    let result = 0;
+
+    switch (operator) {
+        case '+': result = firstNum + secondNum; break;
+        case '-': result = firstNum - secondNum; break;
+        case '*': result = firstNum * secondNum; break;
+        case '/': result = firstNum / secondNum; break;
+    }
+    console.log(result.toFixed(2));
+}
