abe5831df258dcfd7fe9d5e3dbd763494ef36996
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-10-01 23:11:32 +0300

    Add files via upload

--- basic-exe/dayOfWeek.js
+++ basic-exe/dayOfWeek.js
@@ -1,43 +1,20 @@
-function dayOfWeek(input) {
-    let number = Number(input[0]);
+function dayOfWeek(day) {
+    let dayOfWeekAsString = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
 
-    switch (number) {
-        case 1:
-            console.log("Monday");
-            break;
-
-        case 2:
-            console.log("Tuesday");
-            break;
-
-        case 3:
-            console.log("Wednesday");
-            break;
-        case 4:
-            console.log("Thursday");
-            break;
-        case 5:
-            console.log("Friday");
-            break;
-
-        case 6:
-            console.log("Saturday");
-            break;
-
-        case 7:
-            console.log("Sunday");
-            break;
-
-        default:
-            console.log("Error");
-
-            break;
-
-
-
+    let result = dayOfWeekAsString[day - 1];
+    
+    if (result) {
+        console.log(result);
+    } else {
+        console.log("Invalid day!");
     }
-
-
+    
 }
-
-dayOfWeek(["6"])
