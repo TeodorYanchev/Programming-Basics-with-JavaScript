2c9908f2e8bb47fde866200a516b761c3eb06391
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-10-02 20:26:13 +0300

    Add files via upload

---
+++ basic-exe/sumFirstAndLastArrayElements.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastArrayElements(data) {
+    let firstNumber = data[0];
+    let lastElementIndex = data.length - 1;
+    let lastNumber = data[lastElementIndex];
+
+    console.log(firstNumber + lastNumber);
+}
