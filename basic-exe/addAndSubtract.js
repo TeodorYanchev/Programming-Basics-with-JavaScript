4aaf5a93c30d6bd676ff96c25d34e808fb03cc8e
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-10-02 20:26:34 +0300

    Add files via upload

---
+++ basic-exe/addAndSubtract.js
@@ -0,0 +1,21 @@
+function addAndSubtract(input) {
+    let sumOldArray = 0;
+    let sumNewArray = 0;
+    let arrayLength = input.length;
+
+    for (let index = 0; index < arrayLength; index++) {
+        let currentNumber = Number(input[index]);
+        sumOldArray += currentNumber;
+
+        if (currentNumber % 2 === 0) {
+            currentNumber += index;
+        } else {
+            currentNumber -= index;
+        }
+        input[index] = currentNumber;
+        sumNewArray += currentNumber;
+    }
+    console.log(input);
+    console.log(sumOldArray);
+    console.log(sumNewArray);
+}
