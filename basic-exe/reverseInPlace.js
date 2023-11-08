37aefdd87d1b302f5265d51d6356903a4524a3ab
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-11-08 08:52:22 +0200

    Add files via upload

--- basic-exe/reverseInPlace.js
+++ basic-exe/reverseInPlace.js
@@ -1,9 +1,19 @@
 function reverseInPlace(arr) {
-    for(let i = 0; i < arr.length / 2; i++) {
-       let currentEl = arr[i];
-       let nextEL = arr[arr.length - 1 - i];
-       arr[i] = nextEL;
-       arr[arr.length - 1 - i] = currentEl;
+    // for(let i = 0; i < arr.length / 2; i++) {
+    //    let currentEl = arr[i];
+    //    let nextEL = arr[arr.length - 1 - i];
+    //    arr[i] = nextEL;
+    //    arr[arr.length - 1 - i] = currentEl;
+    // }
+    // console.log(arr.join(" "));
+
+    for (let i = 0; i < arr.length / 2; i++) {
+        let j = arr.length - 1 - i;
+
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    console.log(arr.join(" "));
+    console.log(arr.join(' '));
 }
+reverseInPlace(['a', 'b', 'c', 'd', 'e'])
