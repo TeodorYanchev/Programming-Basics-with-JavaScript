dde308cd181f589199ee0064d52083f3474462fe
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-11-14 09:51:28 +0200

    Add files via upload

--- basic-exe/commonElements.js
+++ basic-exe/commonElements.js
@@ -1,9 +1,19 @@
 function commonElements(firstArray, secondArray) {
-    for(let index = 0; index < firstArray.length; index++)  {
-        let currentEl = firstArray[index];
+  // for(let index = 0; index < firstArray.length; index++)  {
+  //     let currentEl = firstArray[index];
 
-        if(secondArray.includes(currentEl)) {
-          console.log(currentEl);
-        }
+  //     if(secondArray.includes(currentEl)) {
+  //       console.log(currentEl);
+  //     }
+  // }
+  for (let el of firstArray) {
+    if(secondArray.includes(el)){
+      console.log(el);
     }
+  }
+
 }
+
+commonElements(['Hey', 'hello', 2, 4, 'Peter', 'e'],
+  ['Petar', 10, 'hey', 4, 'hello', '2']
+)
