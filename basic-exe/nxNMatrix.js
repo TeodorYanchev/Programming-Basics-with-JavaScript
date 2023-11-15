e6dec57e252498a491e8ee06600765859c84024a
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-11-15 10:30:12 +0200

    Add files via upload

--- basic-exe/nxNMatrix.js
+++ basic-exe/nxNMatrix.js
@@ -1,15 +1,21 @@
 function nxNMatrix(n) {
 
-    function rowGenerator() {
-        let row = '';
+    // function rowGenerator() {
+    //     let row = '';
 
-        for (let j = 1; j <= n; j++ ){
-         row += `${n} `
-        }
-        return row;
-    }
+    //     for (let j = 1; j <= n; j++ ){
+    //      row += `${n} `
+    //     }
+    //     return row;
+    // }
     
-    for (let i = 1; i <= n; i++) {
-       console.log(rowGenerator());
+    // for (let i = 1; i <= n; i++) {
+    //    console.log(rowGenerator());
+    // }
+    for(let rowNum = 1; rowNum <= n; rowNum++){
+    let curRow = `${n} `.repeat(n);
+    console.log(curRow);
     }
 }
+
+nxNMatrix(3)
