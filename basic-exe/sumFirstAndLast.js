7dd00bba1b1f0f38a3c239a54412be4dd694892f
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2023-12-26 14:07:28 +0200

    Add files via upload

--- basic-exe/sumFirstAndLast.js
+++ basic-exe/sumFirstAndLast.js
@@ -1,10 +1,19 @@
 function sumFirstAndLast(arr) {
-    
-    if (arr.length === 1) {
-        return console.log(Number(arr.toString()));
-    }
-    
-    let sum = Number(arr.shift()) + Number(arr.pop());
-    console.log(sum);
+   let firstElement = Number(arr.shift());
+   let lastElement = Number(arr.pop());
+   let sum = firstElement + lastElement;
+   return sum;
+    // if (arr.length === 1) {
+    //     return console.log(Number(arr.toString()));
+    // }
 
+    // let sum = Number(arr.shift()) + Number(arr.pop());
+    // console.log(sum);
+
+    // let first = arr.shift();
+    // let last = arr.pop();
+
+    // console.log(Number(first) + Number(last));
 }
+
+sumFirstAndLast(['20', '30', '40'])
