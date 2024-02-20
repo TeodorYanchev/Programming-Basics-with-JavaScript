38fd0c5d3d30a8dbcf15672201b7cbfa45a7aa92
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2024-02-20 10:22:55 +0200

    Add files via upload

--- basic-exe/mergeArrays.js
+++ basic-exe/mergeArrays.js
@@ -12,3 +12,6 @@ function mergeArrays(firstArray, secondArray) {
     }
     console.log(resultArray.join(' - '));
 }
+mergeArrays(['5', '15', '23', '56', '35'],
+    ['17', '22', '87', '36', '11']
+)
