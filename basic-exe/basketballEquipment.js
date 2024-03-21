16b236f43e207a6ef33e9f48c6c8f4dc43a75c2a
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2024-03-21 07:49:00 +0200

    Add files via upload

--- basic-exe/basketballEquipment.js
+++ basic-exe/basketballEquipment.js
@@ -1,15 +1,14 @@
 function basketballEquipment(input) {
-    let annualTax = Number(input[0])
 
-let sneakers = annualTax - (0.4 * annualTax);
-let outfit = sneakers - (0.2 * sneakers);
-let ball = 1/4 * outfit;
-let accessories = 1/5 * ball;
+    let annualFee = Number(input[0]);
 
-let totalPrice = annualTax + sneakers + outfit + ball + accessories;
-console.log(totalPrice);
+    let shoes = annualFee - (annualFee * 0.4);
+    let basketballUniform = shoes - (shoes * 0.2);
+    let ball = 1 / 4 * basketballUniform;
+    let accessories = 1 / 5 * ball;
 
-
+    let totalPrice = annualFee + shoes + basketballUniform + ball + accessories;
+    console.log(totalPrice.toFixed(2));
 }
 
-basketballEquipment(["550 "])
+basketballEquipment(["550"])
