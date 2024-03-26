825d0c7ab1befeeeb6584914aca376ef8f1c3cb4
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2024-03-26 06:24:55 +0200

    Add files via upload

--- basic-exe/godzillaVsKong.js
+++ basic-exe/godzillaVsKong.js
@@ -1,34 +1,29 @@
 function godzillaVsKong(input) {
 
-let budget = Number(input[0]);
-let statisticianCount = Number(input[1]);
-let clothingPricePerOneST = Number(input[2]);
+    let budget = Number(input[0]);
+    let actorCount = Number(input[1]);
+    let priceOfClothingPerActor = Number(input[2]);
 
-let decorPrice = 0.1 * budget;
-let statisticianCloathPrice = statisticianCount * clothingPricePerOneST;
+    let decorPrice = 0.10 * budget;
+    let cloathingPrice = priceOfClothingPerActor * actorCount;
 
-if (statisticianCount > 150) {
-    statisticianCloathPrice = statisticianCloathPrice * 0.9
-}
-let totalPrice = decorPrice + statisticianCloathPrice;
+    if (actorCount > 150) {
+        cloathingPrice *= 0.9;
+    }
 
-if (budget >= totalPrice) {
-    moneyLeft = budget - totalPrice
-    console.log("Action!");
-    console.log(`Wingard starts filming with ${moneyLeft.toFixed(2)} leva left.`);
-    
-} else {
-    let moneyNeeded = totalPrice - budget
-    console.log("Not enough money!");
-    console.log(`Wingard needs ${moneyNeeded.toFixed(2)} leva more.`);
-    
-}
+    let totalPrice = decorPrice + cloathingPrice;
 
+    if (budget >= totalPrice) {
+        let moneyLeft = budget - totalPrice;
+        console.log("Action!");
+        console.log(`Wingard starts filming with ${moneyLeft.toFixed(2)} leva left.`);
+    } else {
+        let moneyNeeded = totalPrice - budget;
+        console.log("Not enough money!");
+        console.log(`Wingard needs ${moneyNeeded.toFixed(2)} leva more.`);
+    }
 }
 
-godzillaVsKong(["9587.88",
-"222",
-"55.68"])
-
-
-
+godzillaVsKong(["9587.88", 
+"222", 
+"55.68"]) 
