7f6acbd42d46f0d1688beba7740b4c4a2894cffa
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2024-03-28 09:10:30 +0200

    Add files via upload

--- basic-exe/club.js
+++ basic-exe/club.js
@@ -1,44 +1,34 @@
 function club(input) {
-    let index = 0;
+    let wishingForProfit = Number(input[0]);
+    let sum = 0;
 
-    let targetIncome = Number(input[index]); // cel na club-a
-    index++;
-
-    let command = input[index];
-    index++;
-
-    let income = 0;  // prihodi na club-a
-
-    while (command !== "Party!") {
-        let cocktailName = command;
-
-        let cocktailCount = Number(input[index]);
-        index++;
-
-        let pricePerCocktail = cocktailName.length;
-        let totalCocktailPrice = cocktailCount * pricePerCocktail;
-
-        if(totalCocktailPrice % 2 !== 0) {
-            totalCocktailPrice *= 0.75;
-        }
-
-        income += totalCocktailPrice;
-
-        if(income >= targetIncome) {
-            console.log("Target acquired.");
+    for (let index = 1; index < input.length; index += 2) {
+        let currentCocktail = input[index];
+        if (currentCocktail === 'Party!') {
+            console.log(`We need ${(wishingForProfit - sum).toFixed(2)} leva more.`);
+            console.log(`Club income - ${sum.toFixed(2)} leva.`);
             break;
         }
+        let cocktailCount = Number(input[index + 1]);
+        let pricePerCocktail = currentCocktail.length;
+        let orderPrice = pricePerCocktail * cocktailCount;
 
+        if (orderPrice % 2 !== 0) {
+            orderPrice *= 0.75;
+        }
 
-        command = input[index];
-        index++;
+        sum += orderPrice;
+        if (sum >= wishingForProfit) {
+            console.log("Target acquired.");
+            console.log(`Club income - ${sum.toFixed(2)} leva.`);
+            break;
+        }
     }
-
-    if(command === "Party!") {
-        let sumNeeded = targetIncome - income;
-        console.log(`We need ${sumNeeded.toFixed(2)} leva more.`);
-    }
-
-    console.log(`Club income - ${income.toFixed(2)} leva.`);
 }
-
+club(["100",
+    "Sidecar",
+    "7",
+    "Mojito",
+    "5",
+    "White Russian",
+    "10"]) 
