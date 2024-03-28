5db5cb7f99c06b3202ca272ed0829c93d1ca86ab
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2024-03-28 09:10:12 +0200

    Add files via upload

--- basic-exe/balls.js
+++ basic-exe/balls.js
@@ -1,55 +1,46 @@
 function balls(input) {
+
     let ballsCount = Number(input[0]);
+    let colour = input[1];
     let points = 0;
-    let redBalls = 0;
-    let orangeBalls = 0;
-    let yellowBalls = 0;
-    let whiteBalls = 0;
-    let otherColourBalls = 0;
-    let blackBallsDiv = 0;
+    let redPoints = 0;
+    let orangePoints = 0;
+    let yellowPoints = 0;
+    let whitePoints = 0;
+    let otherBallPoints = 0;
+    let dividesBlackBalls = 0;
 
-    for (let i = 1; i <= ballsCount; i++) {
-        let colourType = input[i];
+    for (let index = 1; index < ballsCount + 1; index++) {
+        let currentColour = input[index];
 
-        switch (colourType) {
-            case "red": points += 5;
-                redBalls++;
-                break;
-            case "orange": points += 10;
-                orangeBalls++;
-                break;
-            case "yellow": points += 15;
-                yellowBalls++;
-                break;
-            case "white": points += 20;
-                whiteBalls++;
-                break;
-            case "black": points = Math.floor(points / 2);
-                blackBallsDiv++;
-                break;
-            default: points = points;
-                otherColourBalls++;
-                break;
+        if (currentColour === 'red') {
+            points += 5;
+            redPoints++;
+        } else if (currentColour === 'orange') {
+            points += 10;
+            orangePoints++;
+        } else if (currentColour === 'yellow') {
+            points += 15;
+            yellowPoints++;
+        } else if (currentColour === 'white') {
+            points += 20;
+            whitePoints++;
+        } else if (currentColour === 'black') {
+            points = Math.floor(points / 2);
+            dividesBlackBalls++;
+        } else {
+            otherBallPoints++;
         }
-
     }
     console.log(`Total points: ${points}`);
-    console.log(`Red balls: ${redBalls}`);
-    console.log(`Orange balls: ${orangeBalls}`);
-    console.log(`Yellow balls: ${yellowBalls}`);
-    console.log(`White balls: ${whiteBalls}`);
-    console.log(`Other colors picked: ${otherColourBalls}`);
-    console.log(`Divides from black balls: ${blackBallsDiv}`);
+    console.log(`Red balls: ${redPoints}`);
+    console.log(`Orange balls: ${orangePoints}`);
+    console.log(`Yellow balls: ${yellowPoints}`);
+    console.log(`White balls: ${whitePoints}`);
+    console.log(`Other colors picked: ${otherBallPoints}`);
+    console.log(`Divides from black balls: ${dividesBlackBalls}`);
 }
-
-balls(["10",
+balls(["3",
     "white",
-    "white",
-    "ee",
-    "red",
-    "orange",
-    "red",
     "black",
-    "black",
-    "black",
-    "black"]);
+    "pink"])
