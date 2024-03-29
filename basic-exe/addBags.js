07ea154d27fd4dfc64ff01429129b97c101e885d
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2024-03-29 07:49:16 +0200

    Add files via upload

--- basic-exe/addBags.js
+++ basic-exe/addBags.js
@@ -1,40 +1,33 @@
 function addBags(input) {
 
-    let priceForLuggageOver20Kg = Number(input[0]);
-    let weightOfTheLuggage = Number(input[1]);
-    let daysBeforeJourney = Number(input[2]);
+    let priceOfLuggage = Number(input[0]);
+    let kgLuggage = Number(input[1]);
+    let dayBeforeTrip = Number(input[2]);
     let luggageCount = Number(input[3]);
+    let sum = 0;
 
-    let price = 0;
-    let priceIncrease = 0;
-
-    if (weightOfTheLuggage < 10) {
-        price = 0.20 * priceForLuggageOver20Kg;
-    } else if (weightOfTheLuggage <= 20) {
-        price = 0.50 * priceForLuggageOver20Kg;
-    } else {
-        price = priceForLuggageOver20Kg;
+    if (kgLuggage < 10) {
+        sum = 0.2 * priceOfLuggage;
+    } else if (kgLuggage < 21) {
+        sum = 0.5 * priceOfLuggage;
+    } else if (kgLuggage > 20) {
+        sum = priceOfLuggage;
     }
 
-    if (daysBeforeJourney > 30) {
-        priceIncrease = 0.1 * price;
-    } else if (daysBeforeJourney >= 7 && daysBeforeJourney <= 30) {
-        priceIncrease = 0.15 * price;
-    } else {
-        priceIncrease = 0.40 * price;
+    if (dayBeforeTrip > 30) {
+        sum *= 1.1;
+    } else if (dayBeforeTrip >= 7 && dayBeforeTrip <= 30) {
+        sum *= 1.15;
+    } else if (dayBeforeTrip < 7) {
+        sum *= 1.40;
     }
 
-    let sum = price + priceIncrease;
-    let totalSUm = sum * luggageCount;
+    let finalSum = luggageCount * sum;
 
-    console.log(`The total price of bags is: ${totalSUm.toFixed(2)} lv.`);
-
+    console.log(`The total price of bags is: ${finalSum.toFixed(2)} lv.`);
 }
 
-addBags(["63.80",
-    "23",
-    "3",
-    "1"])
-
-
-
+addBags(["63.80", 
+"23", 
+"3", 
+"1"]) 
