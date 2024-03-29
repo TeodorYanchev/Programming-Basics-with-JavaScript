1f58d432180b8bbd5153c8acd17a25f8b355e567
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2024-03-29 07:49:01 +0200

    Add files via upload

--- basic-exe/catWalking.js
+++ basic-exe/catWalking.js
@@ -1,18 +1,22 @@
 function catWalking(input) {
-let minutesOfWalkPerDay = Number(input[0]);
-let numberOfWalk = Number(input[1]);
-let caloricIntake = Number(input[2]);
 
-let minutesOfWalkingPerDay = minutesOfWalkPerDay * numberOfWalk;
-let totalBurned = minutesOfWalkingPerDay * 5;
+    let minutesWalkPerDay = Number(input[0]);
+    let walkingCount = Number(input[1]);
+    let catsCalories = Number(input[2]);
 
-if(totalBurned >= caloricIntake /2){
-    console.log(`Yes, the walk for your cat is enough. Burned calories per day: ${totalBurned}.`);
-} else{
-    console.log(`No, the walk for your cat is not enough. Burned calories per day: ${totalBurned}.`);
+    let timeWalk = minutesWalkPerDay * walkingCount;
+    let caloriesBurned = timeWalk * 5;
+    let neededCalories = catsCalories / 2;
+
+
+    if(caloriesBurned >= neededCalories){
+console.log(`Yes, the walk for your cat is enough. Burned calories per day: ${caloriesBurned}.`);
+    } else {
+        console.log(`No, the walk for your cat is not enough. Burned calories per day: ${caloriesBurned}.`);
+    }
+
 }
-}
 
-catWalking(["15",
-"2",
+catWalking(["15", 
+"2", 
 "500"])
