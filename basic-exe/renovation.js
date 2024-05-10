ffd6d33e8413abbe5ddb2e6670402acab3c47332
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2024-05-10 14:17:46 +0300

    Add files via upload

--- basic-exe/renovation.js
+++ basic-exe/renovation.js
@@ -3,35 +3,42 @@ function renovation(input) {
     let visochinaNaStenata = Number(input[0]);
     let shirinaNaStenata = Number(input[1]);
     let vratiteProzorcite = Number(input[2]);
-    
+
     let allApart = (visochinaNaStenata * shirinaNaStenata * 4);
     let bezVratiIProzortsi = (vratiteProzorcite / 100) * allApart;
     let paintApart = Math.ceil(allApart - bezVratiIProzortsi);
     let currentLitters = paintApart;
-    
+
     for (let index = 3; index < input.length; index++) {
-    let command = Number(input[index]);
-    currentLitters -= command;
-    
-
-    if(input[index + 1] === 'Tired!'){
-console.log(`${currentLitters} quadratic m left.`);
-    }
-
-
-    }
-    if(paintApart >= currentLitters){
-        console.log(`All walls are painted and you have ${Math.abs(currentLitters)} l paint left!`);
-        } else if(currentLitters >= paintApart){
-        console.log(`All walls are painted! Great job, Pesho!`);
+        let command = input[index];
+        if (command === 'Tired!') {
+            console.log(`${Math.abs(currentLitters)} quadratic m left.`);
+            return;
         }
+
+         command = Number(input[index]);
+        currentLitters -= command;
+
+
+        if (input[index + 1] === 'Tired!') {
+            console.log(`${Math.abs(currentLitters)} quadratic m left.`);
+            return;
+        }
+
+
+    }
+    if (paintApart > currentLitters) {
+        console.log(`All walls are painted and you have ${Math.abs(currentLitters)} l paint left!`);
+    } else if (currentLitters >= paintApart) {
+        console.log(`All walls are painted! Great job, Pesho!`);
+    }
 
 }
 
 
-renovation(["2", 
-"3", 
-"25", 
-"6", 
-"7", 
-"8"]) 
+renovation(["2",
+    "3",
+    "25",
+    "6",
+    "7",
+    "8"]) 
