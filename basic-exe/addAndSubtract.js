33943752b3621070386bb7b6b31ffe482809bf21
TeodorYanchev <134443295+TeodorYanchev@users.noreply.github.com>
2024-09-27 11:14:37 +0300

    Add files via upload

--- basic-exe/addAndSubtract.js
+++ basic-exe/addAndSubtract.js
@@ -1,23 +1,14 @@
-function addAndSubtract(input) {
-    let sumOldArray = 0;
-    let sumNewArray = 0;
-    let arrayLength = input.length;
+function addAndSubtract(firstNumber, secondNumber, thirdNumber) {
 
-    for (let index = 0; index < arrayLength; index++) {
-        let currentNumber = Number(input[index]);
-        sumOldArray += currentNumber;
+    function sum(firstNumber, secondNumber) {
+           return firstNumber + secondNumber;
 
-        if (currentNumber % 2 === 0) {
-            currentNumber += index;
-        } else {
-            currentNumber -= index;
-        }
-        input[index] = currentNumber;
-        sumNewArray += currentNumber;
     }
-    console.log(input);
-    console.log(sumOldArray);
-    console.log(sumNewArray);
-}
 
-addAndSubtract([5, 15, 23, 56, 35])
+    let sumResult = sum(firstNumber, secondNumber);
+
+    function subtract(sumResult, thirdNumber) {
+        return sumResult - thirdNumber;
+    }
+    return subtract(sumResult, thirdNumber);
+}
